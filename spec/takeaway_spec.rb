77c8b21e1aa3276aa64bcafcde84ead6dde5f156
Louise <louiselai88@gmail.com>
2014-04-25 15:34:42 +0100

    menu completed and passes tests

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,15 +1,35 @@
 require "takeaway"
 
 	describe "Louise's takeaway shop" do
-	  
-	  it "has a list of dishes" do
-	  	shop = Shop.new
-	  	expect(shop.menu.any?).to be_true
-	  end
 
-	  it "each menu item has a price" do
-	  	shop = Shop.new
-	  	ex
+		context "the menu" do
+
+			let (:shop) {Shop.new}
+		  
+		  it "has dished listed in it" do
+		  	expect(shop.menu.any?).to be_true
+		  end
+
+		  it "each dish has a price" do
+		  	expect(shop.menu[:chicken_laksa_noodles].integer?).to be_true 
+			end
+
 		end
+
+		context "ordering" do
+			
+			it "food can be ordered" do
+				
+			end
+
+		end
+
+
+
+
+
+
+
+
 
 	end
