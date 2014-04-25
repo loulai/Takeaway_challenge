c8f9c2b969bf1c2e96faf5205c5f9a3b192c4828
Louise <louiselai88@gmail.com>
2014-04-25 14:01:59 +0100

    changed the send_sms file to try and work when i want it to

--- send_sms.rb
+++ send_sms.rb
@@ -1,12 +1,27 @@
 require 'rubygems'
 require 'twilio-ruby'
- 
-# Get your Account Sid and Auth Token from twilio.com/user/account
-account_sid = 'AC81dba0a9bd8bb6fc2be33419f028a3ab'
-auth_token = '44d8a2f7650bd77ac7caaae2ca56c8d1'
-@client = Twilio::REST::Client.new account_sid, auth_token
- 
-message = @client.account.sms.messages.create(:body => "Hello Miha",
-    :to => "+447760725881",     # Replace with your phone number
-    :from => "+441353210104")   # Replace with your Twilio number
-puts message.sid
+
+my_string = true
+
+class Shopowner
+
+	def send_text(my_string)
+	 
+	 if my_string == true
+		# Get your Account Sid and Auth Token from twilio.com/user/account
+		account_sid = 'AC81dba0a9bd8bb6fc2be33419f028a3ab'
+		auth_token = '44d8a2f7650bd77ac7caaae2ca56c8d1'
+		@client = Twilio::REST::Client.new account_sid, auth_token
+		 
+		message = @client.account.sms.messages.create(:body => "Hello Miha",
+		    :to => "+447760725881",     # Replace with your phone number
+		    :from => "+441353210104")   # Replace with your Twilio number
+		puts message.sid
+
+		else
+			print "whoops, didn't work, try again"
+		end
+
+	end
+
+end
